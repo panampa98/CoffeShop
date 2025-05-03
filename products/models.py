9032ba176bd552533170845023308f53d4445c83
Pold Anampa <poldanampa@gmail.com>
2025-05-03 03:51:51 -0500

    modified upload to, it its for load directly to media and not in a subfolder

--- products/models.py
+++ products/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     description = models.TextField(max_length=255, verbose_name='Description')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Price')
     available = models.BooleanField(default=True, verbose_name='Available')
-    image = models.ImageField(upload_to='products', null=True, blank=True, verbose_name='Image')
+    image = models.ImageField(upload_to='', null=True, blank=True, verbose_name='Image')
 
     def __str__(self):
         return self.name
